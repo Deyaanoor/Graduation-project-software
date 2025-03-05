57570a2c17791c964ecfa805e8aa0ec74bc62aad
omarzaqout <omar02-2012@hotmail.com>
2025-03-05 17:29:13 +0200

    test ssl

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');  // استيراد حزمة CORS
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 app.use(cors());  // إضافة middleware الخاص بـ CORS
 
