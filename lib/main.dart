e0476a6e74cfaf0975e4b9da0bd781dc4116273e
Deyaanoor <127193661+Deyaanoor@users.noreply.github.com>
2025-03-06 17:18:22 +0200

    Update environment variables, dependencies, and add BezierContainer widget; remove employee-related files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/screens/auth/welcomePage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +19,17 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
+          bodyLarge: GoogleFonts.montserrat(textStyle: textTheme.bodyLarge),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter + Riverpod + Node.js',
-      home: HomeScreen(),
+      home: WelcomePage(),
     );
   }
 }
