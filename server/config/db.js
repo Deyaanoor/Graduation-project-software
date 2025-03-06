b3881caf617a718d204ddf24795435bb5fa76d3e
omarzaqout <61704101+omarzaqout@users.noreply.github.com>
2025-03-06 15:32:07 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,18 @@
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
-console.log("deyaa")
+
 dotenv.config(); // تحميل المتغيرات من ملف .env
-console.log("omar")
+
 const connectDB = async () => {
   try {
     const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-    console.log("✅ Connected to the database");
-    return client.db(); // إرجاع قاعدة البيانات
+    
+    const dbName = process.env.DB_NAME || "prosoftware"; // اسم القاعدة من .env أو افتراضيًا
+    const db = client.db(dbName);
+
+    console.log(`✅ Connected to the database: ${db.databaseName}`);
+    return db;
   } catch (error) {
     console.error("❌ Error connecting to database:", error);
     throw new Error("Failed to connect to the database");
