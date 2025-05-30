ee88c14634af0336a57461b7304724dad7f57977
omarzaqout <omar02-2012@hotmail.com>
2025-05-30 23:14:59 +0300

    feat: Add Firebase service account configuration

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -3,8 +3,10 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-// const serviceAccount = require(path.resolve(__dirname, "./firebase-key.json"));
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require(path.resolve(
+  __dirname,
+  "./graduation-notifications-firebase-adminsdk-fbsvc-32e2c8fc0b"
+));
 
 if (!admin.apps.length) {
   admin.initializeApp({
