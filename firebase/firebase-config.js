d5c54ba795084e53f92d68a5487053d1b0b8c4c9
omarzaqout <omar02-2012@hotmail.com>
2025-05-30 22:42:34 +0300

    style: Standardize quotation marks in firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,14 +1,15 @@
 // firebase/firebase-config.js
 
-const admin = require('firebase-admin');
-const path = require('path');
+const admin = require("firebase-admin");
+const path = require("path");
 
-const serviceAccount = require(path.resolve(__dirname, './firebase-key.json'));
+const serviceAccount = require(path.resolve(__dirname, "./firebase-key.json"));
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("✅ Firebase admin initialized");
 }
 
 module.exports = admin;
