efac9e09f762a7f1a3e9ffda840f1ed9f026fac4
omarzaqout <omar02-2012@hotmail.com>
2025-05-30 23:28:18 +0300

    test env

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -7,11 +7,13 @@ const serviceAccount = require(path.resolve(
   __dirname,
   "./graduation-notifications-firebase-adminsdk-fbsvc-32e2c8fc0b"
 ));
+const serviceAccountt = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountt),
   });
+  console.log("serviceAccountt :", serviceAccountt);
   console.log("✅ Firebase admin initialized");
 }
 
