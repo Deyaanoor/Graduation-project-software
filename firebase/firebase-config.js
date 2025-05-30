b032cc5245615c1404535d9badced5ee78122747
omarzaqout <omar02-2012@hotmail.com>
2025-05-31 00:22:07 +0300

    notification

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,19 +1,26 @@
-// firebase/firebase-config.js
-
 const admin = require("firebase-admin");
-const path = require("path");
+// const path = require("path");
+// require("dotenv").config({ path: path.resolve(__dirname, "../assets/.env") });
 
-const serviceAccount = require(path.resolve(
-  __dirname,
-  "./graduation-notifications-firebase-adminsdk-fbsvc-32e2c8fc0b"
-));
-const serviceAccountt = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = {
+//   type: process.env.FIREBASE_TYPE,
+//   project_id: process.env.FIREBASE_PROJECT_ID,
+//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+//   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//   client_id: process.env.FIREBASE_CLIENT_ID,
+//   auth_uri: process.env.FIREBASE_AUTH_URI,
+//   token_uri: process.env.FIREBASE_TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+//   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+//   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+// };
+const serviceAccount = require("/etc/secrets/firebaseServiceAccount.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountt),
+    credential: admin.credential.cert(serviceAccount),
   });
-  console.log("serviceAccountt :", serviceAccountt);
   console.log("✅ Firebase admin initialized");
 }
 
