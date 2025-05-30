5e36666d24c6b51f89d9d2a1f1c8a087bbdeae76
omarzaqout <omar02-2012@hotmail.com>
2025-05-30 22:57:54 +0300

    test noti

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -3,7 +3,8 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require(path.resolve(__dirname, "./firebase-key.json"));
+// const serviceAccount = require(path.resolve(__dirname, "./firebase-key.json"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
