e0f6ffc09ac97b381530e36882b5823ac197fcbc
omarzaqout <omar02-2012@hotmail.com>
2025-06-09 23:39:42 +0300

    fix: Add logging for existing user verification in verifyEmail function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -186,10 +186,12 @@ const verifyEmail = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: "Account already verified" });
     }
+    console.log("existingUser", existingUser);
+    console.log("IDD", existingUser._id);
+    console.log("___ID decoded", decoded._id);
 
     // أدخل المستخدم مع حقل isVerified true
     const newUser = {
-      _id: decoded._id,
       name: decoded.name,
       email: decoded.email,
       password: decoded.password,
